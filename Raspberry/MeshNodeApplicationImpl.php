e22e7f2d6eb6d66368c37131aa6613f820e888ed
sebbe33 <seb.blomberg@gmail.com>
2015-04-11 16:37:21 +0200

    Added PHP mesh network interface

---
+++ Raspberry/MeshNodeApplicationImpl.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'MeshNodeApplicationImpl.php';
+
+class MeshNodeApplicationImpl implements MeshNodeApplication {
+    private $id, $name;
+    
+    public function __construct($id, $name) {
+        $this->id = $id;
+        $this->name = $name;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+    
+}
